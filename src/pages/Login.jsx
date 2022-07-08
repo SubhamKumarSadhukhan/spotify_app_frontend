552170a4a9f0bad5b60e5b2825b320e61f351460
0x-Lucifer <subham@0xytocin.com>
2022-07-09 01:18:03 +0530

    login register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Card, Link, Container, Typography } from '@mui/material';
+// hooks
+import useResponsive from '../hooks/useResponsive';
+// sections
+import { LoginForm } from '../sections/auth/login';
+
+const RootStyle = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
+}));
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  padding: theme.spacing(0, 0),
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Login() {
+  const smUp = useResponsive('up', 'sm');
+  return (
+      <RootStyle>
+        <Container maxWidth="sm">
+          <ContentStyle>
+            <Typography variant="h4" gutterBottom>
+              Sign in
+            </Typography>
+            <Typography sx={{ color: 'text.secondary', mb: 1 }}>Enter your details below.</Typography>
+            <LoginForm />
+              <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+                Don’t have an account?{' '}
+                <Link variant="subtitle2" component={RouterLink} to="/register">
+                  Get started
+                </Link>
+              </Typography>
+          </ContentStyle>
+        </Container>
+      </RootStyle>
+  );
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { styled } from '@mui/material/styles';
+import { Card, Link, Container, Typography } from '@mui/material';
+// hooks
+import useResponsive from '../hooks/useResponsive';
+// components
+// sections
+import { RegisterForm } from '../sections/auth/register';
+// ----------------------------------------------------------------------
+
+const RootStyle = styled('div')(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex',
+  },
+}));
+
+const SectionStyle = styled(Card)(({ theme }) => ({
+  width: '100%',
+  maxWidth: 464,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  margin: theme.spacing(2, 0, 2, 2),
+}));
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Register() {
+  const smUp = useResponsive('up', 'sm');
+  return (
+      <RootStyle>
+
+        <Container>
+          <ContentStyle>
+            <Typography variant="h4" gutterBottom>
+              Register Here
+            </Typography>
+            <RegisterForm />
+            <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
+              By registering, I agree to Aqionline&nbsp;
+              <Link underline="always" color="text.primary" href="#">
+                Terms of Service
+              </Link>
+              {''}and{''}
+              <Link underline="always" color="text.primary" href="#">
+                Privacy Policy
+              </Link>
+              .
+            </Typography>
+
+            {!smUp && (
+              <Typography variant="body2" sx={{ mt: 3, textAlign: 'center' }}>
+                Already have an account?{' '}
+                <Link variant="subtitle2" to="/login" component={RouterLink}>
+                  Login
+                </Link>
+              </Typography>
+            )}
+          </ContentStyle>
+        </Container>
+      </RootStyle>
+  );
+}
