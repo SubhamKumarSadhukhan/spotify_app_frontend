7264c184e7c91548c3464b38a13dbdcb9423a5b2
0x-Lucifer <subham@0xytocin.com>
2022-07-11 12:08:01 +0530

    rating done

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,16 +1,27 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 let baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5001";
 export const getuser = () => async (dispatch) => {
   try {
-    const sid = Cookies.get("connect.sid");
-    if (sid) {
-      const { data } = await axios.get("/api/users/getuser", {
-        withCredentials: true,
-      });
-      dispatch({ type: "LOGIN", payload: data });
-    } else dispatch({ type: "SET_USER_CHECK" });
+    console.log("lol");
+    const token = Cookies.get("Authorization");
+    const decoded = jwt_decode(token, { complete: true });
+    console.log(decoded);
+    if (Date.now() <= decoded.exp * 1000) {
+      axios.defaults.headers.common["Authorization"] = token;
+      try {
+        const { data } = await axios.get(baseURL + "/api/getuser", {});
+        axios.defaults.headers.common["Authorization"] = data.token;
+        Cookies.set("Authorization", data.token, { path: "/" });
+        dispatch({ type: "LOGIN", payload: { name: decoded.name } });
+      } catch (e) {
+        dispatch({ type: "LOGOUT" });
+      }
+    } else {
+      dispatch({ type: "SET_USER_CHECK" });
+    }
   } catch (err) {
     dispatch({ type: "SET_USER_CHECK" });
   }
@@ -47,7 +58,7 @@ export const gettop10songs = (values) => async (dispatch) => {
     toast.error(error.response && error.response.data);
   }
 };
-export const gettop10artists = (values) => async (dispatch) => {
+export const gettop10artists = () => async (dispatch) => {
   try {
     const { data } = await axios.get(baseURL + "/api/gettop10artists", {
       withCredentials: true,
@@ -59,11 +70,46 @@ export const gettop10artists = (values) => async (dispatch) => {
 };
 export const ratesong = (values) => async (dispatch) => {
   try {
+    console.log("values");
     const { data } = await axios.post(baseURL + "/api/ratesong", values, {
       withCredentials: true,
     });
     toast.success(data);
   } catch (error) {
+    toast.error(error.response && error.response.data);
+  }
+};
+export const getartists = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get(baseURL + "/api/getartists", {
+      withCredentials: true,
+    });
+    dispatch({ type: "SET_ARTISTS", payload: data });
+  } catch (error) {
+    toast.error(error.response && error.response.data);
+  }
+};
+export const addsong = (values) => async (dispatch) => {
+  try {
+    console.log("values");
+    const { data } = await axios.post(baseURL + "/api/addsong", values, {
+      withCredentials: true,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    toast.success(data);
+  } catch (error) {
+    toast.error(error.response && error.response.data);
+  }
+};
+export const addartist = (values) => async (dispatch) => {
+  try {
+    const { data } = await axios.post(baseURL + "/api/addartist", values, {
+      withCredentials: true,
+    });
+    dispatch(getartists());
+    toast.success(data);
+  } catch (error) {
+    console.log(error.message);
     toast.error(error.response && error.response.data);
   }
 };
